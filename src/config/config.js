4e9d7988f88e37c9a9a1213c04359e6e8e755058
muthamizhyadav <muthamizhyadav@gmail.com>
2025-03-12 14:25:54 +0530

    asda

--- src/config/config.js
+++ src/config/config.js
@@ -62,8 +62,8 @@ module.exports = {
     from: envVars.EMAIL_FROM,
   },
   s3: {
-    secret: envVars.AWS_SECRET_KEY,
-    access: envVars.AWS_ACCESS_KEY,
-    region:envVars.AWS_REGION
+    secret:'',
+    access: '',
+    region:''
   },
 };
